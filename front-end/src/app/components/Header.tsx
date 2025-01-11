b2ae046ccae162c386fa9f23ced0a4628ddb0569
lucianoZgabriel <lucianozanin@gmail.com>
2025-01-11 16:09:57 -0400

    chore: front end initial setup

---
+++ front-end/src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Bitcoin Explorer</h1>
+      </div>
+    </header>
+  );
+}
